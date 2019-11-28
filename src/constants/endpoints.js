f8b467fbadc9030892ce7997ca3be05f0598ec45
Emmanuel NWAMAIFE <emmanuel.nwamaife@interswitchgroup.com>
2019-11-28 23:42:01 +0100

    chore(endpoint): update default api base-url

--- src/constants/endpoints.js
+++ src/constants/endpoints.js
@@ -1,5 +1,5 @@
-// const url = 'https://teamwork-app-api-stage.herokuapp.com/api/v1';
-const url = 'http://localhost:3500/api/v1';
+const url = 'https://teamwork-app-api-stage.herokuapp.com/api/v1';
+// const url = 'http://localhost:3500/api/v1';
 
 export default {
     signIn: `${url}/auth/signin`
