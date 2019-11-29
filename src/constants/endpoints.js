1d8392e4fbaa51fd8cb105c278ce10e10192f4ab
Emmanuel NWAMAIFE <emmanuel.nwamaife@interswitchgroup.com>
2019-11-29 01:25:42 +0100

    chore(url): add more endpoint urls for backend

--- src/constants/endpoints.js
+++ src/constants/endpoints.js
@@ -2,5 +2,9 @@ const url = 'https://teamwork-app-api-stage.herokuapp.com/api/v1';
 // const url = 'http://localhost:3500/api/v1';
 
 export default {
-    signIn: `${url}/auth/signin`
+    signIn: `${url}/auth/signin`,
+    signUp: `${url}/auth/create-user`,
+    feeds: `${url}/feed`,
+    articles: `${url}/articles`,
+    gifs: `${url}/gifs`,
 };
