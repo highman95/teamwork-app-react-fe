e2339ccbe3c164a55427c818a46703da39f47d59
Emmanuel NWAMAIFE <emmanuel.nwamaife@interswitchgroup.com>
2019-11-29 23:48:29 +0100

    feature(post): implement components for post display [Delivers #8, #9]

---
+++ src/components/posts/Comments.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Comment.css';
+
+class Comments extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isLoading: false,
+            comments: []
+        };
+    }
+
+    componentDidMount() {
+        const { comments } = this.props;
+        this.setState({ comments });
+    }
+
+    render() {
+        const { isLoading } = this.state;
+        const { comments } = this.props;
+        const commentsMap = comments && comments.map((comment) => <Comment key={comment.commentId} comment={comment} />);
+
+        return (
+            <>
+                {isLoading && <div className="loading-box">Loading Comments...</div>}
+                <div className="post-comments-box">
+                    {commentsMap}
+                </div>
+            </>
+        );
+    }
+}
+
+Comments.propTypes = {
+    comments: PropTypes.array
+}
+
+const Comment = ({ comment }) => {
+    return (
+        <div className="comment-wrapper">
+            <div className="author-box">User &mdash; {comment.authorId}</div>
+            <div className="comment-box">{comment.comment}</div>
+        </div>);
+};
+
+export default Comments;
